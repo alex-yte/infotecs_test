69697d48224f3797890a163bfc7c78786902e0dc
Denis <den.alex.chirkov@gmail.com>
2022-02-20 22:14:50 +0300

    table sorted

--- main.js
+++ main.js
@@ -16,19 +16,67 @@ const p = new Promise (function (resolve,reject) {
 
 p.then (db=>{
     const dataBase = db;
-    buildTable(dataBase)
-    function buildTable(data){
-        let table = document.querySelector('.people')
-        for (i=0; i<data.length; i++){
-            let row = 
-                `<tr>
-                    <td class="first-col">${data[i].name.firstName}</td>
-                    <td class="second-col">${data[i].name.lastName}</td>
-                    <td class="third-col">${data[i].about}</td>
-                    <td class="fourth-col" style = "background-color: ${data[i].eyeColor}; color: white">${data[i].eyeColor}</td>
-                </tr>`
-            
-        table.innerHTML += row;
-        }
-    }
+    buildTable(dataBase);
 })
+
+p.then(db=>{
+    const dataBase = db;
+    sortTable(dataBase);
+})
+
+function buildTable(data){
+    let table = document.querySelector('.table-body');
+    for (i=0; i<data.length; i++){
+        let row = 
+            `<tr class='rows'>
+                <td class="first-col">${data[i].name.firstName}</td>
+                <td class="second-col">${data[i].name.lastName}</td>
+                <td class="third-col">${data[i].about}</td>
+                <td class="fourth-col" style = "background-color: ${data[i].eyeColor}; color: white">${data[i].eyeColor}</td>
+            </tr>`
+    table.innerHTML += row;
+    }
+}
+
+function sortTable (){
+    const sortingButtons = document.querySelector('thead');
+
+    sortingButtons.addEventListener('click', (e)=>{
+        let table = document.querySelector('tbody');
+        const el = e.target;
+        const dataBase = db;
+        console.log(el.cellIndex);
+        if (el.cellIndex === 0){
+            console.log('firstName');
+            dataBase.sort((a, b) => a.name.firstName > b.name.firstName ? 1 : -1);
+            console.log(table);
+            console.log(dataBase);
+            document.querySelectorAll(".rows").forEach(e => e.remove());
+            buildTable(dataBase);
+        }
+        if (el.cellIndex === 1) {
+            console.log('lastName');
+            dataBase.sort((a, b) => a.name.lastName > b.name.lastName ? 1 : -1);
+            console.log(table);
+            document.querySelectorAll(".rows").forEach(e => e.remove());
+            console.log(dataBase);
+            buildTable(dataBase);
+        }
+        if (el.cellIndex === 2) {
+            console.log('about');
+            dataBase.sort((a, b) => a.about > b.about ? 1 : -1);
+            console.log(table);
+            document.querySelectorAll(".rows").forEach(e => e.remove());
+            console.log(dataBase);
+            buildTable(dataBase);
+        }
+        if (el.cellIndex === 3) {
+            console.log('eyeColor');
+            dataBase.sort((a, b) => a.eyeColor > b.eyeColor ? 1 : -1);
+            console.log(table);
+            document.querySelectorAll(".rows").forEach(e => e.remove());
+            console.log(dataBase);
+            buildTable(dataBase);
+        }
+    })
+}
